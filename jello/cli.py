15eb147647091948a201cb400dc7bcf2a2f7962c
Kelly Brazil <kellyjonbrazil@gmail.com>
2021-05-24 08:14:46 -0700

    version bump to 1.2.11

--- jello/cli.py
+++ jello/cli.py
@@ -14,7 +14,7 @@ from pygments.lexers import JsonLexer
 from pygments.formatters import Terminal256Formatter
 
 
-__version__ = '1.2.10'
+__version__ = '1.2.11'
 
 color_map = {
     'black': ('ansiblack', '\33[30m'),
