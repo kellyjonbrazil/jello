9b2e9377e16afa33ba6961657113bb37edba607f
Kelly Brazil <kellyjonbrazil@gmail.com>
2021-06-29 09:52:28 -0700

    update comments

--- build-package.sh
+++ build-package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# build jc PIP package
-# to install locally, run:   pip3 install jc-x.x.tar.gz
+# build jello PIP package
+# to install locally, run:   pip3 install jello-x.x.tar.gz
 
 python3 setup.py sdist bdist_wheel
