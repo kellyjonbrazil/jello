6061938378914d27ecd3de5060f7b95a43629281
Kelly Brazil <kellyjonbrazil@gmail.com>
2023-01-14 12:42:52 -0800

    add dict methods to valid variable name test

--- jello/lib.py
+++ jello/lib.py
@@ -24,7 +24,18 @@ except Exception:
 
 
 def is_valid_variable_name(name: str) -> bool:
-    return name.isidentifier() and not iskeyword(name)
+    dict_methods = [
+        '__class__', '__class_getitem__', '__contains__', '__delattr__',
+        '__delitem__', '__dir__', '__eq__', '__format__', '__ge__',
+        '__getattribute__', '__getitem__', '__getstate__', '__gt__',
+        '__init__', '__init_subclass__', '__ior__', '__iter__', '__le__',
+        '__len__', '__lt__', '__ne__', '__new__', '__or__', '__reduce__',
+        '__reduce_ex__', '__repr__', '__reversed__', '__ror__', '__setattr__',
+        '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'clear',
+        'copy', 'fromkeys', 'get', 'items', 'keys', 'pop', 'popitem',
+        'setdefault', 'update', 'values'
+    ]
+    return name.isidentifier() and not iskeyword(name) and name not in dict_methods
 
 
 class opts:
