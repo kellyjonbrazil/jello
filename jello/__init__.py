ca33e0db5c577c6af81e3da56bbb0600e8719e55
Kelly Brazil <kellyjonbrazil@gmail.com>
2023-05-20 11:22:40 -0700

    doc update

--- jello/__init__.py
+++ jello/__init__.py
@@ -1,7 +1,7 @@
 """jello - query JSON at the command line with python syntax"""
 
 
-__version__ = '1.6.0'
+__version__ = '1.6.1'
 AUTHOR = 'Kelly Brazil'
 WEBSITE = 'https://github.com/kellyjonbrazil/jello'
 COPYRIGHT = '© 2020-2023 Kelly Brazil'
